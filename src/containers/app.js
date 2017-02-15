fe2caf02be52e0e270b21957735d22ccf3d0a469
Oleksandr Sachuk <ovsachuk@gmail.com>
2017-02-15 16:59:38 +0200

    redux simple starter

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Name from '../components/name';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
       <div className="container">
